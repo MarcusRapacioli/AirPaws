c5338b765539a6d8a836df34f578274c35b31c33
yuichiro toyama <ytoyama1872@gmail.com>
2018-08-20 17:22:55 +0100

    validations added - debugging pending

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: 'User', foreign_key: 'user_id'
   belongs_to :booking
   belongs_to :reviewable, polymorphic: true
+
+  validates :body, presence: :true
 end
