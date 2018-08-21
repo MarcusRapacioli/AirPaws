f36a0641ba2a1331da3b02f99aaac885cbe9a530
Wesley Sprague <wesleysprague@yahoo.com>
2018-08-21 17:35:50 +0100

    created puppies index, with cards and puppy information, started work on search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,13 @@
 
 
 User.create!(first_name: "prince", last_name: "william", email: "will@me.com", password: "moolah123")
-User.create!(first_name: "prince", last_name: "harry", email: "harry@me.com", password: "moolah123")
+User.create!(first_name: "prince", last_name: "harry", email: "harry@me.com", password: "whater")
+User.create!(first_name: "prince", last_name: "john", email: "john@me.com", password: "moolah123")
+User.create!(first_name: "prince", last_name: "trump", email: "trump@me.com", password: "moolah123")
+User.create!(first_name: "prince", last_name: "beyonce", email: "bey@me.com", password: "moolah123")
+User.create!(first_name: "prince", last_name: "50 Cent", email: "fiddy@me.com", password: "moolah123")
 Puppy.create!(name: "Vincent", breed: "poodle", price: 25000, age: 12, location: "london", user_id: 1)
+Puppy.create!(name: "queen", breed: "lab", price: 23, age: 2, location: "london", user_id: 2)
+Puppy.create!(name: "megan", breed: "pug", price: 1, age: 12, location: "the shire", user_id: 3)
+Puppy.create!(name: "james cordon", breed: "bulldog", price: 250500, age: 12, location: "bangkok", user_id: 4)
+Puppy.create!(name: "lil whiskers ", breed: "auzzy shepard", price: 25000, age: 12, location: "china", user_id: 5)
