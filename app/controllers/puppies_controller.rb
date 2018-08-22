4afab9444157eec7a061252febe1751d0bb95e7e
Georges-Alexandre Haines <georgeshaines@gmail.com>
2018-08-22 14:38:16 +0100

    Index not working

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,6 +1,7 @@
 class PuppiesController < ApplicationController
 
   def index
+    @puppies = Puppy.all
     if params[:breed] && params[:age] && params[:location]
       @puppies = Puppy.where("breed iLIKE ? AND age = '#{params[:age]}' AND location = '#{params[:location]}'", "%#{params[:breed]}%")
        # iLIKE ? # , "%#{params[:breed]}%")
