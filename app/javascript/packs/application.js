9297706cb9150db416f0cadefaabab2f27971c54
yuichiro toyama <ytoyama1872@gmail.com>
2018-08-24 10:14:15 +0100

    sweet alert and tabs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,7 @@ import "bootstrap";
 
 import { blurryScroll } from '../components/banner';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { bindSweetAlertButtonDemo } from '../components/puppyForm';
 blurryScroll();
 initUpdateNavbarOnScroll();
+bindSweetAlertButtonDemo();
