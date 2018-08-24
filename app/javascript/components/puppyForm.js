c81e79d52d09c38e508f2aab7c351259a075071f
yuichiro toyama <ytoyama1872@gmail.com>
2018-08-24 13:57:21 +0100

    sweet alert fixed

--- app/javascript/components/puppyForm.js
+++ app/javascript/components/puppyForm.js
@@ -8,7 +8,6 @@ function bindSweetAlertButtonDemo() {
         title: "Puppy booking succesfull",
         text: "Have a wonderful time with a wonderful pup",
         icon: "success"
-        button: "Go to Dashboard"
       });
     });
   }
